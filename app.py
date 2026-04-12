70c377d81d2119786b5b1eafcb9080e78ac8846b
amandabzcosta <131314762+amandabzcosta@users.noreply.github.com>
2026-04-12 15:27:41 -0300

    Implement LGPD assistant using Streamlit and LangChain

---
+++ app.py
@@ -0,0 +1,88 @@
+import streamlit as st
+import os
+from langchain_community.document_loaders import PyMuPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_chroma import Chroma
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+
+st.set_page_config(page_title="Assistente LGPD", page_icon="⚖️", layout="centered")
+
+st.title("⚖️ Assistente LGPD - RAG")
+st.write("Faça perguntas sobre a Lei Geral de Proteção de Dados.")
+
+try:
+    os.environ["GOOGLE_API_KEY"] = st.secrets["GOOGLE_API_KEY"]
+except KeyError:
+    st.error("Chave de API não encontrada. Configure o st.secrets no Streamlit.")
+    st.stop()
+
+@st.cache_resource(show_spinner="Processando o documento LGPD...")
+def load_rag_pipeline():
+    file = "Lei_geral_protecao_dados_pessoais_1ed.pdf"
+    
+    if not os.path.exists(file):
+        st.error(f"Arquivo {file} não encontrado na pasta do projeto!")
+        st.stop()
+
+    loader = PyMuPDFLoader(file)
+    docs = loader.load()
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        chunk_size=1000, chunk_overlap=200
+    )
+    texts = text_splitter.split_documents(docs)
+    
+    embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+    vector_store = Chroma.from_documents(
+        documents=texts,
+        embedding=embeddings,
+    )
+    
+    model = ChatGoogleGenerativeAI(
+        model="gemini-flash-lite-latest",
+        temperature=0,  
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+    )
+
+    prompt = ChatPromptTemplate.from_messages([
+        ("system","Você é um assistente da LGPD. Use o seguinte contexto para responder: {context}"),
+        ("human", "{input}"),
+    ])
+    
+    retriever = vector_store.as_retriever(search_kwargs={"k": 3})
+    question_answer_chain = create_stuff_documents_chain(model, prompt)
+    rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+    
+    return rag_chain
+
+rag_chain = load_rag_pipeline()
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if question := st.chat_input("Digite sua dúvida sobre a LGPD..."):
+    
+    with st.chat_message("user"):
+        st.markdown(question)
+    st.session_state.messages.append({"role": "user", "content": question})
+
+    with st.chat_message("assistant"):
+        with st.spinner("Buscando na lei e formulando resposta..."):
+            try:
+                response = rag_chain.invoke({"input": question})
+                answer = response["answer"]
+                st.markdown(answer)
+                
+                st.session_state.messages.append({"role": "assistant", "content": answer})
+                
+            except Exception as e:
+                st.error(f"Ocorreu um erro ao processar sua pergunta: {e}")
